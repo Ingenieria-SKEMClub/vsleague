cb2c57a820229551136eafa4e93c88e2b25159e8
Daniel Barrantes <djbarrantesrodriguez@gmail.com>
2020-07-16 14:42:01 -0600

    ya está

--- scripts/information.js
+++ scripts/information.js
@@ -1,1 +1,3 @@
 ReactDOM.render(<Menu/>, document.getElementById("menu-container"))
+ReactDOM.render(<Content/>, document.getElementById("content-container"))
+ReactDOM.render(<Sponsors/>, document.getElementById("sponsors"))
