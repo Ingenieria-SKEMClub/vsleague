40e6a00438daf97f5c0ac8a3f679b4acd392eec5
André <Andre>
2020-07-17 02:14:23 -0600

    andre changes

--- scripts/information.js
+++ scripts/information.js
@@ -1,3 +1,42 @@
-ReactDOM.render(<Menu/>, document.getElementById("menu-container"))
-ReactDOM.render(<Content/>, document.getElementById("content-container"))
-ReactDOM.render(<Sponsors/>, document.getElementById("sponsors"))
+class Content extends React.Component {
+    render(){
+        return(
+            <div className="content-container">
+                <div class="item1"><img src="images/character.png" className="character"></img></div>
+                <div class="item2"></div>
+            </div>
+        );
+    };
+};
+class Sponsors extends React.Component {
+    render(){
+        return(
+            <div className="sponsors">
+                <p>PATROCINADO POR</p>
+                <img src="images/sponsor1.png" className="sponsorimg"></img>
+                <img src="images/sponsor2.png" className="sponsorimg"></img>
+            </div>
+        );
+    };
+};
+class Information extends React.Component{
+    componentDidMount(){
+        document.getElementById("loadingFrame").style.animation = "loadingEnd 1s forwards"
+    }
+    render(){
+        return(
+            <div>
+                <div className='menu-container'>
+                    <Menu />
+                </div>
+                <div id='content-container'>
+                    <Content />
+                </div>
+                <footer id='sponsors'>
+                    <Sponsors/>
+                </footer>
+            </div>
+        );
+    };
+};
+ReactDOM.render(<Information/>, document.getElementById("app"))
